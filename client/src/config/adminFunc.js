8608c8dd490c022c1d8772f9bab59f5fbf1ab438
Marcel Humes <info@mdevelopment.com>
2019-03-04 17:42:41 -0500

    adding key

---
+++ client/src/config/adminFunc.js
@@ -0,0 +1,14 @@
+const adminFunc = myKey => {
+
+        if (myKey === '5c676e52151b7c1bc2d50b83') {
+        
+            return true;
+           
+        } else {
+          return false;
+        }
+  
+}
+
+    
+export default adminFunc;
