33d1f43be06cfa1ebe8a7d8fb41df1405ce661d6
Marcel Humes <info@mdevelopment.com>
2021-02-15 17:29:06 -0500

    Config files

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://MarcelHumes:udt777@mdevelopment-testing-shard-00-00.xgwts.mongodb.net:27017,mdevelopment-testing-shard-00-01.xgwts.mongodb.net:27017,mdevelopment-testing-shard-00-02.xgwts.mongodb.net:27017/mdevelopment_testing?ssl=true&replicaSet=atlas-uo2dny-shard-0&authSource=admin&retryWrites=true&w=majority",
-    secretOrKey: 'secret'
+    mongoURI: process.env.MONGO_URI,
+    secretOrKey: process.env.SECRET_OR_KEY
 };
